d342c8f8e72209ea7b77e100659fe956fdb0e4b6
Leo Mata <lmata@weareconflict.com>
2026-07-11 18:08:54 -0600

    chore: remove committed tsc output for api scripts/configs and ignore the patterns

--- apps/api/scripts/export-schema.js
+++
@@ -1,8 +0,0 @@
-import { printSchema } from "graphql";
-import { writeFileSync } from "fs";
-import { schema } from "../src/graphql/schema";
-const sdl = printSchema(schema);
-writeFileSync("schema.graphql", sdl);
-console.log("Schema exported to apps/api/schema.graphql");
-console.log(`${sdl.split("\n").length} lines`);
-//# sourceMappingURL=export-schema.js.map
--- apps/api/scripts/scaffold.d.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env tsx
-export {};
-//# sourceMappingURL=scaffold.d.ts.map
